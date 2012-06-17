8d997aea131f56f94498c81f946e930e1827f9d3
Patrick Breheny <patrick.breheny@uky.edu>
2012-06-16 21:39:28 -0400

    genData: added documentation; now allows scalar beta

--- R/genData.R
+++ R/genData.R
@@ -1,32 +1,53 @@
+#' Generates a random design matrix and outcome.
+#'
+#' This function generated a random design matrix \code{X} and
+#' response vector \code{y} for use in regression testing/simulation.
+#'
+#' @param n Sample size
+#' @param J Number of groups
+#' @param K Number of explanatory variables per group
+#' @param beta Vector of regression coefficients in the generating model, or, if a scalar, the value of each nonzero regression coefficient.
+#' @param family Generate \code{y} according to linear (\code{"gaussian"})
+#' or logistic (\code{"binomial"}) model
+#' @param J0 Number of nonzero groups
+#' @param K0 Number of nonzero coefficients per group
+#' @param SNR Signal to noise ratio
+#' @param sig Should the groups be heterogeneous (in beta) or homogeneous
+#' @param sig.g Should the coefficients within a group be heterogeneous or homogeneous
+#' @param rho Correlation between groups
+#' @param rho.g Correlation between parameters within a group
+#' 
+#' @export
 genData <- function(n, J, K=1, beta, family=c("gaussian","binomial"), J0=ceiling(J/2), K0=K, SNR=1, sig = c("homogeneous","heterogeneous"), sig.g = c("homogeneous","heterogeneous"), rho = 0, rho.g = rho)
-  {
-    family <- match.arg(family)
-    sig <- match.arg(sig)
-    sig.g <- match.arg(sig.g)
+{
+  family <- match.arg(family)
+  sig <- match.arg(sig)
+  sig.g <- match.arg(sig.g)
     
-    ## Gen X, S
-    X <- genX(n=n,J=J,K=K,rho=rho,rho.g=rho.g)
-    S <- matrix(rho,nrow=J*K,ncol=J*K)
-    for (i in 1:J) S[(i-1)*K+1:K,(i-1)*K+1:K] <- rho.g
-    diag(S) <- rep(1,J*K)
+  ## Gen X, S
+  X <- genX(n=n,J=J,K=K,rho=rho,rho.g=rho.g)
+  S <- matrix(rho,nrow=J*K,ncol=J*K)
+  for (i in 1:J) S[(i-1)*K+1:K,(i-1)*K+1:K] <- rho.g
+  diag(S) <- rep(1,J*K)
 
-    ## Gen beta
+  ## Gen beta
+  if (missing(beta) | length(beta)==1) {
     j <- rep(1:J,rep(K,J))
-    if (missing(beta))
-      {
-        k <- rep(1:K,J)
-        b <- (j <= J0) * (k <= K0)
-        if (sig=="heterogeneous") b <- b*j
-        if (sig.g=="heterogeneous") b <- b*k
-        s <- c(1,-1)[1+j%%2] * c(1,-1)[1+k%%2]
-        b <- b*s
-        beta <- b*sqrt(SNR)/sqrt(crossprod(b,S)%*%b)
-      }
-
-    ## Gen y
-    y <- genY(X%*%beta, family=family, sigma=1)
-    return(list(X=X,y=y,beta=beta,family=family,group=j))
+    k <- rep(1:K,J)
+    b <- (j <= J0) * (k <= K0)
+    s <- c(1,-1)[1+j%%2] * c(1,-1)[1+k%%2]
+    if (missing(beta)) {
+      if (sig=="heterogeneous") b <- b*j
+      if (sig.g=="heterogeneous") b <- b*k
+      b <- b*s
+      beta <- b*sqrt(SNR)/sqrt(crossprod(b,S)%*%b)
+    } else beta <- b*s*beta
   }
+
+  ## Gen y
+  y <- genY(X%*%beta, family=family, sigma=1)
+  return(list(X=X,y=y,beta=beta,family=family,group=j))
+}
 
 ## rho  : correlation across all explanatory variables
 ## rho.g: correlation within group (must be at least rho)
