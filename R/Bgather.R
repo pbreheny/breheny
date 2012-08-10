505f593f5bb3ab17da879ae19a6694fdc9e9cac7
Patrick Breheny <patrick.breheny@uky.edu>
2012-08-10 12:49:03 -0400

    More fixes for Bsave/Bgather

--- R/Bgather.R
+++ R/Bgather.R
@@ -1,43 +1,40 @@
-Bgather <- function(suffix="",safe=FALSE)
+Bgather <- function (suffix = "", safe = FALSE) 
 {
   require(abind)
-
-  ## Find files
   all.files <- list.files()
-  if (suffix=="") pattern <- "tmp[[:alnum:]]+\\.RData" else {
-    pattern <- paste("tmp[[:alnum:]]+-",suffix,"\\.RData",sep="")
-  }
-  matching.files <- all.files[grep(pattern,all.files)]
+  Suffix <- if (nchar(suffix) > 0) paste("-",suffix,sep="") else suffix
+  pattern <- paste("tmp[[:alnum:]]+", Suffix, "\\.RData", sep = "")
+  matching.files <- all.files[grep(pattern, all.files)]
   
-  ## Join objects
   res <- NULL
-  types <- c("numeric","character","array","matrix")
+  types <- c("numeric", "character", "array", "matrix")
   for (f in matching.files) {
     e <- new.env()
-    load(f,envir=e)
-    res.f <- get("res",envir=e)
+    load(f, envir = e)
+    res.f <- get("res", envir = e)
     rc <- class(res)
-    if (rc=="list") {
+    if (rc == "list") {
       rc <- sapply(res.f, class)
       for (i in 1:length(rc)) {
-        ##el <- names(rc)[i]
         if (rc[i] %in% types) {
-          res[[i]] <- abind(res[[i]],res.f[[i]],along=1)
-        } else res[[i]] <- res.f[[i]]
+          res[[i]] <- abind(res[[i]], res.f[[i]], along = 1)
+        }
+        else res[[i]] <- res.f[[i]]
       }
-    } else {
+      names(res) <- names(res.f)
+    }
+    else {
       if (rc %in% types) {
-        res <- abind(res,res.f,along=1)
-      } else res <- res.f      
+        res <- abind(res, res.f, along = 1)
+      }
+      else res <- res.f
     }
   }
-  
-  ## Destroy
   if (!safe) {
-    if (suffix=="") fn <- "res.RData" else fn <- paste(suffix,".RData",sep="")
-    save(res,file=fn)
+    save(res, file = paste(Sys.Date(), Suffix, ".RData", 
+                sep = ""))
     for (f in matching.files) {
-      system(paste("rm",f))
+      system(paste("rm", f))
     }
   }
   invisible(res)
--- R/Bsave.R
+++ R/Bsave.R
@@ -5,6 +5,6 @@ Bsave <- function(res,suffix="")
   if (length(NCA)) {
     save(res,file=paste("tmp",NCA[1], Suffix,".RData",sep=""))
   } else {
-    save(res,file=paste("res", Suffix,".RData",sep=""))
+    save(res,file=paste(Sys.Date(), Suffix,".RData",sep=""))
   }
 }
