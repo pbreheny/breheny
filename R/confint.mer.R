99fbf3b49fb0dfc4978ca2dcfbf8e12abe2b2dd3
Patrick Breheny <patrick.breheny@uky.edu>
2012-08-16 17:18:18 -0400

    rstring: Added options for numbers/specials; confint.mer: added 'simple' option

--- R/confint.mer.R
+++ R/confint.mer.R
@@ -1,19 +1,25 @@
-confint.mer <- function(object, parm, level = 0.95, n.sim = 10000, ...)
+confint.mer <- function(object, parm, level = 0.95, n.sim = 10000, method=c("simple", "mcmc"), ...)
 {
-  ## Sample
-  if (missing(parm)) parm <- 1:length(object@fixef)
+  method <- match.arg(method)
   varnames <- names(object@fixef)[parm]
-  samp <- lme4:::mcmcsamp(object, n.sim)
-
-  ## CI
-  est <- apply(samp@fixef, 1, mean)
-  stderr <- apply(samp@fixef, 1, sd)
   alpha <- 1-level
-  CI <- t(sapply(parm, function(i) {quantile(samp@fixef[i,], probs = c(alpha/2, 1 - alpha/2))}))
-  rownames(CI) <- varnames
+  if (missing(parm)) parm <- 1:length(object@fixef)
+  
+  if (method=="mcmc") {
+    samp <- lme4:::mcmcsamp(object, n.sim)
+    est <- apply(samp@fixef, 1, mean)
+    stderr <- apply(samp@fixef, 1, sd)
+    CI <- t(sapply(parm, function(i) {quantile(samp@fixef[i,], probs = c(alpha/2, 1 - alpha/2))}))
+    pval <- sapply(parm, function(i) {mean(samp@fixef[i,]>0)})
+    pval <- ifelse(pval <= 0.5, 2 * pval, 2 * (1 - pval))    
+  } else {
+    summ <- summary(object)
+    df <- summ@ngrps
+    CI <- cbind(summ@coefs[parm,1] - qt(1-alpha/2, df)*summ@coefs[parm,2],
+                summ@coefs[parm,1] + qt(1-alpha/2, df)*summ@coefs[parm,2])
+    pval <- 2*(1-pt(abs(summ@coefs[parm,3]), df))
+  }
 
-  ## pval
-  pval <- sapply(parm, function(i) {mean(samp@fixef[i,]>0)})
-  pval <- ifelse(pval <= 0.5, 2 * pval, 2 * (1 - pval))
+  rownames(CI) <- varnames
   cbind(CI,pval)
 }
--- R/rstring.R
+++ R/rstring.R
@@ -1,8 +1,11 @@
-rstring <- function(n=1, length=20)
+rstring <- function(n=1, length=20, include.extra=FALSE, include.num=FALSE)
 {
   val <- character(n)
+  pool <- c(letters,LETTERS)
+  if (include.extra) pool <- c(pool, c("!","@","#","$","%","^","^","&","*","(",")"))
+  if (include.num) pool <- c(pool, 0:9)
   for (i in 1:n) {
-    val[i] <- paste(sample(c(letters,LETTERS), length, replace=TRUE), collapse="")
+    val[i] <- paste(sample(pool, length, replace=TRUE), collapse="")
   }
   val
 }
