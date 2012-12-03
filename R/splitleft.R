1adee85b151e3a5ae1bc0a856c6a8ac23ae7d940
Patrick Breheny <patrick.breheny@uky.edu>
2012-12-03 15:54:12 -0500

    splitleft, splitright now accept ...

--- R/splitleft.R
+++ R/splitleft.R
@@ -1,7 +1,7 @@
 splitleft <- function(x, split, ...) {
   spl <- strsplit(x, split=split, ...)
   n <- length(spl)
-  X <- matrix(unlist(spl),nrow=n,byrow=TRUE)
+  X <- matrix(unlist(spl), nrow=n, byrow=TRUE)
   return(X[,1])
 }
 splitright <- function(x, split, ...) {
