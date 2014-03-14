b42e9e5c26b8ed2dee55aa96717ab941b8de4dae
Patrick Breheny <patrick-breheny@uiowa.edu>
2014-03-14 16:55:40 -0500

    Bsave/Bgather bug fix

--- R/Bsave.R
+++ R/Bsave.R
@@ -2,6 +2,7 @@ Bsave <- function(res, suffix="") {
   Suffix <- if (nchar(suffix) > 0) paste("-",suffix,sep="") else suffix
   NCA <- as.numeric(commandArgs(TRUE))
   name <- as.character(match.call()[2])
+  if (name != make.names(name)) name <- "res"
   if (length(NCA)) {
     save(res, name, file=paste("tmp",NCA[1], Suffix,".RData",sep=""))
   } else {
