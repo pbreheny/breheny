3bbc8ae0b5835909021551bf640b6991305f1a3c
Patrick Breheny <patrick-breheny@uiowa.edu>
2015-02-19 14:27:08 -0600

    myTrellis: better defaults for histograms/bar plots

--- R/myTrellis.R
+++ R/myTrellis.R
@@ -1,6 +1,13 @@
 myTrellis <- function(n.col=3, lwd=3, alpha=1, cex=1) {
   col <- if (n.col==2) pal(3, alpha=alpha)[c(1,3)] else pal(n.col, alpha=alpha)
+  if (n.col==2) {
+    solidCol <- hcl(seq(15, 375, len = 4), l = 70, c = 100)[c(1,3)]
+  } else {
+    solidCol <- hcl(seq(15, 375, len = n.col+1), l = 70, c = 100)[1:n.col]
+  }
   trellis.par.set(superpose.line=list(lty=1, lwd=3, col=col),
-                  strip.background=list(col="gray95"),
-                  superpose.symbol=list(pch=19, col=col))
+                  strip.background=list(col="gray90"),
+                  superpose.symbol=list(pch=19, col=col),
+                  plot.polygon=list(border="white", col="#8FC7FF", lwd=1),
+                  superpose.polygon=list(col=solidCol))
 }
