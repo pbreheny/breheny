c616d0cf8185ae14155e09f2ac128eb7e94004ed
Patrick Breheny <patrick-breheny@uiowa.edu>
2015-06-11 11:42:55 -0500

    Added hdpower for high-dimensional/gene expression power calcs

--- R/hdpower.R
+++ R/hdpower.R
@@ -5,18 +5,19 @@ hdpower <- function(n, FDR, p0, D=1, sig=1, n1, n2) {
     df <- n1 + n2 - 2
   } else {
     df <- n - 2
+    n1 <- n2 <- n/2
   }
   p1 <- 1-p0
-  ncp <- sqrt(n/2)*D/sig
+  ncp <- D / sig * 1/sqrt(1/n1 + 1/n2)
   
   # Solve for CV satisfying FDR constraint
   f <- function(CV) {
-    fp <- p0*(1-pt(CV, 2*n-2))
-    tp <- p1*(1-pt(CV, 2*n-2, ncp))
+    fp <- p0*(1-pt(CV, df))
+    tp <- p1*(1-pt(CV, df, ncp))
     fp/(fp+tp) - FDR
   }
   CV <- uniroot(f, c(0, 10))$root
   
   # Return sensitivity
-  1-pt(CV, 2*n-2, ncp)
+  1-pt(CV, df, ncp)
 }
