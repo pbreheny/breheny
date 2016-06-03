cda8e603d27276dfe6c84ee80761ca1cbf763099
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-06-03 13:25:24 -0500

    splitl/r handles NUlls better

--- R/splitleft.R
+++ R/splitleft.R
@@ -1,10 +1,12 @@
 splitleft <- function(x, split, ...) {
+  if (!length(x)) return(character(0))
   spl <- strsplit(x, split=split, ...)
   n <- length(spl)
   X <- matrix(unlist(spl), nrow=n, byrow=TRUE)
   return(X[,1])
 }
 splitright <- function(x, split, ...) {
+  if (!length(x)) return(character(0))
   spl <- strsplit(x, split=split, ...)
   n <- length(spl)
   X <- matrix(unlist(spl),nrow=n,byrow=TRUE)
