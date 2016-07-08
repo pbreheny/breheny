e8fff8a557f5f4bc5e9ff8b43a3557761edf3f8f
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-07-08 10:45:48 -0500

    jags2bugs: removed unnecessary line of code

--- R/jags2bugs.R
+++ R/jags2bugs.R
@@ -1,6 +1,6 @@
 convertJAGS <- function(fit) {
   require(coda)
   MCMC <- as.mcmc.list(fit)                ## Convert to coda format
-  require(R2jags)
+  #suppressMessages(require(R2jags))
   R2jags:::mcmc2bugs(MCMC, program="jags") ## Convert to R2jags format
 }
