c1f9ecca0f58e733a0b060da0785c71612c43858
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-07-12 11:48:46 -0500

    Minor indentation/style changes

--- R/pal.R
+++ R/pal.R
@@ -1,7 +1,8 @@
-pal <- function(n, alpha=1)
-{
+pal <- function(n, alpha=1) {
   if (n==2) {
     val <- hcl(seq(15,375,len=4), l=60, c=150, alpha=alpha)[c(1,3)]
-  } else val <- hcl(seq(15,375,len=n+1), l=60, c=150, alpha=alpha)[1:n]
+  } else {
+    val <- hcl(seq(15,375,len=n+1), l=60, c=150, alpha=alpha)[1:n]
+  }
   val
 }
