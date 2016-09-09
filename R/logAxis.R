8d7930a9bc1cb67825116d291b150ac2bddcba04
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-09-09 16:28:31 -0500

    Bug fix: logAxis

--- R/logAxis.R
+++ R/logAxis.R
@@ -1,18 +1,15 @@
-logAxis <- function(side=1, base, disp=base, n=5, style=NULL, ...) {
+logAxis <- function(side=1, base, disp=base, n=5, style="dec", ...) {
   if (missing(base)) base <- exp(1)
   is.x <- side%%2 == 1
   usr <- if(is.x) par("usr")[1:2] else par("usr")[3:4]
   r <- usr*log(base)/log(disp)
   px <- pretty(r, n=n)
   px <- px[px > r[1] & px < r[2]]
-  if (is.null(style)) style <- if (isTRUE(all.equal(px, as.integer(px)))) "pow" else "dec"
   if (style=="pow") {
     at <- px*log(disp)/log(base)
-    lab <- disp^px
+    lab <- sapply(px, function(x){bquote(.(disp)^.(x))})
   } else {
-    s <- seq(usr[1], usr[2], len=n)
-    s <- s - s[which.min(abs(s))]
-    at <- s/max(range(s)/usr[1:2])
+    at <- seq(usr[1], usr[2], len=n)
     lab <- formatC(base^at, digits=1, format="f")
   }
   a <- axis(side, at=at, labels=lab, las=1, ...)
