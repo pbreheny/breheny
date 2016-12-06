dc9d5f6534d915e46fe7c6d4b1628868b82ffcfa
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-12-05 22:35:09 -0600

    nrisk: New function, for n in risk set for survfit

---
+++ R/nrisk.R
@@ -0,0 +1,14 @@
+nrisk <- function(fit, cex=0.8) {
+  usr <- par("usr")
+  x <- axisTicks(usr[1:2], FALSE)
+  xl <- tail(x, 1)
+  x <- x[-length(x)]
+  s <- summary(fit, x)
+  ind1 <- s$strata == levels(s$strata)[1]
+  ind2 <- s$strata == levels(s$strata)[2]
+  axis(3, line=0, at=s$time[ind1], label=s$n.risk[ind1], tick=FALSE, cex.axis=cex)
+  axis(3, line=1, at=s$time[ind2], label=s$n.risk[ind2], tick=FALSE, cex.axis=cex)
+  lab <- gsub(".*\\=", "", levels(s$strata))
+  axis(3, line=0, at=xl, label=lab[1], tick=FALSE, xpd=TRUE, cex.axis=cex)
+  axis(3, line=1, at=xl, label=lab[2], tick=FALSE, xpd=TRUE, cex.axis=cex)
+}
