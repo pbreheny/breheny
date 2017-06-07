aafd5dc0a6e3ef10f88d8d0f400465560629467c
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-06-07 10:18:00 -0500

    write.delim: moving away from row.names

--- R/write.delim.R
+++ R/write.delim.R
@@ -1,4 +1,8 @@
-write.delim <- function(x, file, sep="\t", ...) {
-  cat(sep, file=file)
-  suppressWarnings(write.table(x, file=file, append=TRUE, sep=sep, ...))
+write.delim <- function(x, file, row.names=FALSE, ...) {
+  if (row.names) {
+    cat('\t', file=file)
+    suppressWarnings(write.table(x, file=file, append=TRUE, sep='\t', quote=FALSE, ...))
+  } else {
+    write.table(x, file=file, sep='\t', row.names=FALSE, quote=FALSE, ...)
+  }
 }
