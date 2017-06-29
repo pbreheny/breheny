dd410aba6cb93f22b5424c3b032ec797d8abc5de
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-06-29 11:19:31 -0500

    Added plotl, plotL

---
+++ R/plotl.R
@@ -0,0 +1,24 @@
+plotL <- function(x, l, ...) {
+  if (is.matrix(l)) {
+    L <- apply(l, 2, function(x) x/max(x))
+    matplot(x, L, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+            las=1, col=pal(ncol(L)), lwd=3, lty=1)
+    if (!is.null(colnames(L))) toplegend(legend=colnames(L), lwd=3, col=pal(ncol(L)))
+  } else {
+    l <- l/max(l)
+    plot(x, l, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+         las=1, col=pal(2)[2], lwd=3, ...)
+  }
+}
+plotl <- function(x, l, ...) {
+  if (is.matrix(l)) {
+    L <- apply(l, 2, function(x) x - max(x))
+    matplot(x, L, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+            las=1, col=pal(ncol(L)), lwd=3, lty=1)
+    if (!is.null(colnames(L))) toplegend(legend=colnames(L), lwd=3, col=pal(ncol(L)))
+  } else {
+    l <- l - max(l)
+    plot(x, l, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+         las=1, col=pal(2)[2], lwd=3, ...)
+  }
+}
