4816a1b73ce4fa55a0fa4c3b3857e8fac66ead9c
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-08-22 08:56:02 -0500

    plotl: allows you to change xlab/ylab

--- R/plotl.R
+++ R/plotl.R
@@ -1,24 +1,24 @@
-plotL <- function(x, l, ...) {
+plotL <- function(x, l, xlab=expression(theta), ylab=expression(L(theta)), bty='n', ...) {
   if (is.matrix(l)) {
     L <- apply(l, 2, function(x) x/max(x))
-    matplot(x, L, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+    matplot(x, L, type='l', xlab=xlab, ylab=ylab, bty=bty,
             las=1, col=pal(ncol(L)), lwd=3, lty=1)
     if (!is.null(colnames(L))) toplegend(legend=colnames(L), lwd=3, col=pal(ncol(L)))
   } else {
     l <- l/max(l)
-    plot(x, l, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+    plot(x, l, type='l', xlab=xlab, ylab=ylab, bty=bty,
          las=1, col=pal(2)[2], lwd=3, ...)
   }
 }
-plotl <- function(x, l, ...) {
+plotl <- function(x, l, xlab=expression(theta), ylab=expression(L(theta)), bty='n', ...) {
   if (is.matrix(l)) {
     L <- apply(l, 2, function(x) x - max(x))
-    matplot(x, L, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+    matplot(x, L, type='l', xlab=xlab, ylab=ylab, bty=bty,
             las=1, col=pal(ncol(L)), lwd=3, lty=1)
     if (!is.null(colnames(L))) toplegend(legend=colnames(L), lwd=3, col=pal(ncol(L)))
   } else {
     l <- l - max(l)
-    plot(x, l, type='l', xlab=expression(theta), ylab=expression(L(theta)),
+    plot(x, l, type='l', xlab=xlab, ylab=ylab, bty=bty,
          las=1, col=pal(2)[2], lwd=3, ...)
   }
 }
