dd032b08604089fec7ea4deecc599fc3659acd32
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-08-31 08:15:56 -0500

    Plot.survfit: fixes for no-strata case

--- R/Plot.R
+++ R/Plot.R
@@ -7,14 +7,21 @@ Plot.rpart <- function(obj, ...) {
   fit <- as.party(obj)
   plot(fit, ip_args=list(id=FALSE), tp_args=list(id=FALSE), ...)
 }
-Plot.survfit <- function(obj, legend=c("top", "right", "none"), xlab="Time", ylab="Survival", ...) {
+Plot.survfit <- function(obj, legend=c("top", "right", "none"), xlab="Time", ylab="Survival", conf.int=FALSE, ...) {
   legend <- match.arg(legend)
   n <- length(obj$strata)
-  plot(obj, xlab=xlab, ylab=ylab, bty="n", las=1, col=pal(n), lwd=3, ...)
-  if (legend == "top") {
-    toplegend(legend=gsub('.*=', '', names(obj$strata)), col=pal(n), lwd=3)
-  } else if (legend == "right") {
-    rightlegend(legend=gsub('.*=', '', names(obj$strata)), col=pal(n), lwd=3)
+  if (n == 0) {
+    col <- pal(2)[2]
+  } else {
+    col <- pal(n)
+  }
+  plot(obj, xlab=xlab, ylab=ylab, bty="n", las=1, col=col, lwd=3, conf.int=conf.int, ...)
+  if (n > 0) {
+    if (legend == "top") {
+      toplegend(legend=gsub('.*=', '', names(obj$strata)), col=pal(n), lwd=3)
+    } else if (legend == "right") {
+      rightlegend(legend=gsub('.*=', '', names(obj$strata)), col=pal(n), lwd=3)
+    }
   }
 }
 Plot <- function(obj,...) UseMethod("Plot")
