63d1049c236989af688bba1daca8b081f313b39f
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-02-23 17:46:28 -0600

    Fixed bug in epower w/ unequal sample size as vector arg

--- R/epower.R
+++ R/epower.R
@@ -1,43 +1,37 @@
-epower <- function(n,r,alpha=.05,w=c(1,1),n1,n2,lam,cens)
-  {
-    if (!missing(lam)) r <- lam[1]/lam[2]
-    if (!missing(n1) & !missing(n2))
-      {
-        n <- n1+n2
-        nn <- length(n)
-        W <- matrix(c(n1,n2),byrow=TRUE,ncol=2,nrow=nn)
-      }
-    else
-      {
-        nn <- length(n)
-        W <- matrix(w,byrow=TRUE,ncol=2,nrow=nn)
-      }
-    for (i in 1:nn) W[i,] <- W[i,]/sum(W[i,])
-    
-    if (!missing(cens)) N <- n*W*lam/(lam+cens)
-    else N <- W*n
-    
-    C <- qnorm(1-alpha/2)
-    d <- abs(log(r)/sqrt(apply(1/N,1,sum)))
-    if (!missing(cens) & nn==1) cat(paste("Group ",1:2,": ",round(nn)," events\n",sep="",collapse=""))
-    return(1-pnorm(C,mean=d)+pnorm(-C,mean=d))
+epower <- function(n,r,alpha=.05,w=c(1,1),n1,n2,lam,cens) {
+  if (!missing(lam)) r <- lam[1]/lam[2]
+  if (!missing(n1) & !missing(n2)) {
+    n <- n1+n2
+    nn <- length(n)
+    W <- matrix(c(n1,n2), ncol=2,nrow=nn)
+  } else {
+    nn <- length(n)
+    W <- matrix(w,byrow=TRUE,ncol=2,nrow=nn)
   }
-esamsize <- function(r,w=c(1,1),alpha=.05,power=.8,lam,cens)
-  {
-    w <- w/sum(w)
-    if (!missing(lam)) r <- lam[1]/lam[2]
-    a <- w[1]/w[2]
-    nn <- numeric(2)
-    nn[1] <- (1+a)*(qnorm(1-alpha/2)+qnorm(power))^2/(log(r))^2
-    nn[2] <- nn[1]/a
-    if (!missing(cens))
-      {
-        nn <- nn*(lam+cens)/lam
-      }
-    nn <- ceiling(nn)
-    if (sd(nn) < .0000001) cat("Group: ",nn[1],"\n",sep="")
-    else cat(paste("Group ",1:2,": ",nn,"\n",sep="",collapse=""))
-    if (!missing(cens)) cat("Total n: ",sum(nn),"\n",sep="")
-    else cat("Total events: ",sum(nn),"\n",sep="")
-    return(invisible(sum(nn)))
+  for (i in 1:nn) W[i,] <- W[i,]/sum(W[i,])
+  
+  if (!missing(cens)) N <- n*W*lam/(lam+cens)
+  else N <- W*n
+  
+  C <- qnorm(1-alpha/2)
+  d <- abs(log(r)/sqrt(apply(1/N,1,sum)))
+  if (!missing(cens) & nn==1) cat(paste("Group ",1:2,": ",round(nn)," events\n",sep="",collapse=""))
+  return(1-pnorm(C,mean=d)+pnorm(-C,mean=d))
+}
+esamsize <- function(r,w=c(1,1),alpha=.05,power=.8,lam,cens) {
+  w <- w/sum(w)
+  if (!missing(lam)) r <- lam[1]/lam[2]
+  a <- w[1]/w[2]
+  nn <- numeric(2)
+  nn[1] <- (1+a)*(qnorm(1-alpha/2)+qnorm(power))^2/(log(r))^2
+  nn[2] <- nn[1]/a
+  if (!missing(cens)) {
+    nn <- nn*(lam+cens)/lam
   }
+  nn <- ceiling(nn)
+  if (sd(nn) < .0000001) cat("Group: ",nn[1],"\n",sep="")
+  else cat(paste("Group ",1:2,": ",nn,"\n",sep="",collapse=""))
+  if (!missing(cens)) cat("Total n: ",sum(nn),"\n",sep="")
+  else cat("Total events: ",sum(nn),"\n",sep="")
+  return(invisible(sum(nn)))
+}
