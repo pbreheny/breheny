6c29b4fa05f3c055217c88eef30f43c2604a0369
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-06-15 09:59:43 -0500

    checkData: now works with tibbles/data.tables

--- R/checkData.R
+++ R/checkData.R
@@ -1,23 +1,23 @@
 checkData <- function(Data) {
   for (j in 1:ncol(Data)) {
     cat("Variable: ",names(Data)[j],"\n",sep="")
-    if (sum(!is.na(Data[,j])) == 0) {
+    if (sum(!is.na(Data[[j]])) == 0) {
       cat("All values NA\n")
       readline("Move on to next variable <press enter>:")
       next
     }
-    if (is.numeric(Data[,j])) {
-      print(range(Data[,j],na.rm=TRUE))
-      Hist(Data[,j], n=nrow(Data)/10, xlab=names(Data)[j], main=paste0("Missing: ",sum(is.na(Data[,j]))))
+    if (is.numeric(Data[[j]])) {
+      print(range(Data[[j]],na.rm=TRUE))
+      Hist(Data[[j]], n=nrow(Data)/10, xlab=names(Data)[j], main=paste0("Missing: ",sum(is.na(Data[[j]]))))
     }
-    if (class(Data[,j])[1]=="POSIXlt" | class(Data[,j])[1]=="Date") {
-      print(range(Data[,j],na.rm=TRUE))
+    if (class(Data[[j]])[1]=="POSIXlt" | class(Data[[j]])[1]=="Date") {
+      print(range(Data[[j]],na.rm=TRUE))
       op <- par(fg="white")
-      hist(Data[,j], breaks=nrow(Data)/10, col="gray", las=1, freq=TRUE, xlab=names(Data)[j],
-           main=paste0("Missing: ",sum(is.na(Data[,j]))))
+      hist(Data[[j]], breaks=nrow(Data)/10, col="gray", las=1, freq=TRUE, xlab=names(Data)[j],
+           main=paste0("Missing: ",sum(is.na(Data[[j]]))))
       par(op)
     }
-    if (is.character(Data[,j]) | is.factor(Data[,j]) | is.logical(Data[,j])) print(table(Data[,j],useNA="ifany"))
+    if (is.character(Data[[j]]) | is.factor(Data[[j]]) | is.logical(Data[[j]])) print(table(Data[[j]],useNA="ifany"))
     readline("Move on to next variable <press enter>:")
   }
 }
