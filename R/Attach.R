507334993f1e1ccdba9da3a9dc85701bbe02f126
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-11-05 09:20:49 -0600

    Updating documentation (roxy); removing array2df (obsolete)

--- R/Attach.R
+++ R/Attach.R
@@ -1,9 +1,15 @@
-Attach <- function(x,name=deparse(substitute(x)),...)
-  {
-    if (is.element(name,search()))
-      {
-        pos <- match(name,search())
-        detach(pos=pos)
-      }
-    attach(x,name=name,...)
+#' Detach before attaching
+#'
+#' @examples
+#' Attach(airquality)
+#' Attach(airquality)   # Ordinarily this would provoke warnings
+#'
+#' @export
+
+Attach <- function(x, name=deparse(substitute(x)), ...) {
+  if (is.element(name, search())) {
+    pos <- match(name, search())
+    detach(pos=pos)
   }
+  attach(x, name=name, ...)
+}
---
+++ R/table2df.R
@@ -0,0 +1,26 @@
+#' Convert a table to a (disaggregated) data frame / table
+#'
+#' Note that as.data.frame can do this, but you get aggregated rows with a `Freq` column
+#'
+#' @param Tab   The table
+#'
+#' @examples
+#' table2dt(Titanic)
+#' head(table2df(Titanic))
+#'
+#' @export
+
+table2dt <- function(Tab) {
+  tmp <- as.data.table(Tab)
+  tmp[rep(1:nrow(tmp), tmp$N),-ncol(tmp)]
+}
+
+#' @describeIn table2dt   Convert a table to a (disaggregated) data frame
+#' @export
+
+table2df <- function(Tab) {
+  tmp <- as.data.frame(Tab)
+  tmp <- tmp[rep(1:nrow(tmp), tmp$Freq),-ncol(tmp)]
+  rownames(tmp) <- NULL
+  tmp
+}
