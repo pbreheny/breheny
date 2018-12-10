d343025b54c906553b087566289e699e368708db
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-12-10 13:45:42 -0600

    bioc() exported and doc'd

--- R/bioc.R
+++ R/bioc.R
@@ -1,3 +1,11 @@
+#' Install bioconductor packages
+#'
+#' @examples
+#' \dontrun{
+#' bioc('RnaSeqSampleSize')}
+#'
+#' @export
+
 bioc <- function (...) {
   source("https://bioconductor.org/biocLite.R")
   biocLite(...)
