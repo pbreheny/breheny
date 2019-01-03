b8f4e40092272848f09535adebd655e0f2438f89
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-01-03 16:49:14 -0600

    roxy logAxis

--- R/logAxis.R
+++ R/logAxis.R
@@ -1,3 +1,22 @@
+#' Make a log-scale axis
+#'
+#' @param side    1=bottom, 2=left, 3=top, 4=right
+#' @param base    What base is the log?  Default is base e (natural log).
+#' @param disp    By default, displays in same base as how constructed, but can display in other bases too
+#' @param n       Number of tick marks
+#' @param style   Either "dec" for decimals such as 0.125, or "pow" for powers such as 2^(-3)
+#' @param ...     Further arguments to `axis()`
+#'
+#' @examples
+#' x <- rexp(100)
+#' y <- runif(100)
+#' plot(log(x), y)
+#' plot(log(x), y, xaxt='n', xlab='x')
+#' logAxis()
+#' plot(log(x), y, xaxt='n', xlab='x')
+#' logAxis(style='pow', disp=2)
+#'
+#' @export
 logAxis <- function(side=1, base, disp=base, n=5, style="dec", ...) {
   if (missing(base)) base <- exp(1)
   is.x <- side%%2 == 1
