cf17b99790bde511a57490850b27988210c2ef39
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-03-14 09:51:42 -0500

    Fixed bug in check()

--- R/check.R
+++ R/check.R
@@ -20,7 +20,7 @@ check <- function(x, y, check.attributes=FALSE, ...) {
   if (class(checkResult)[1]=="logical") return(TRUE)
   xname <- gsub("()", "", match.call()[2])
   yname <- gsub("()", "", match.call()[3])
-  if (!exists(.test)) .test <- ''
+  if (!exists('.test')) .test <- ''
   message <- paste0("Problem in ", .test, "\n", xname, " not equal to ", yname, "\n", checkResult)
   stop(message, call.=FALSE)
 }
