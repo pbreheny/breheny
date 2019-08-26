ed81d3855423ef301d95cd8caeb6c0bc5e7a18ad
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-08-26 11:47:47 -0500

    Exporting and documenting rbern()

--- R/rbern.R
+++ R/rbern.R
@@ -1,3 +1,15 @@
+#' Generate random bernoulli (non-homogeneous binomial) variates
+#'
+#' Returns the sum of bernoulli variables with different probabilities.
+#'
+#' @param N      Number of random variates to generate.
+#' @param prob   Vector of probabilities.
+#'
+#' @examples
+#' rbern(5, c(.3, .6, .1))
+#'
+#' @export
+
 rbern <- function(N,prob) {
   n <- length(prob)
   X <- matrix(rbinom(N*n,1,prob),nrow=n,ncol=N)
