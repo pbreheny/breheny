ee76d47cfe35170e5b9dd3caa33ed3237feb3223
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-10-14 08:00:22 -0500

    checkData: Added documentation; exported

--- R/checkData.R
+++ R/checkData.R
@@ -1,13 +1,27 @@
+#' Quick inspection of columns in a data frame / table for weirdness
+#'
+#' Provides histograms and ranges for numeric features, and tables for categorical features, along with missing data information.
+#'
+#' @param Data   A data frame (or data table)
+#'
+#' @examples
+#' checkData(airquality)
+#'
+#' @export
+
 checkData <- function(Data) {
+  cat("Number of instances: ", nrow(Data), "\n")
+  cat("Number of features: ", ncol(Data), "\n")
+
   for (j in 1:ncol(Data)) {
-    cat("Variable: ",names(Data)[j],"\n",sep="")
+    cat("Variable: ", names(Data)[j],"\n", sep="")
     if (sum(!is.na(Data[[j]])) == 0) {
       cat("All values NA\n")
       readline("Move on to next variable <press enter>:")
       next
     }
     if (is.numeric(Data[[j]])) {
-      print(range(Data[[j]],na.rm=TRUE))
+      cat('  Range:', range(Data[[j]], na.rm=TRUE), '\n')
       Hist(Data[[j]], n=nrow(Data)/10, xlab=names(Data)[j], main=paste0("Missing: ",sum(is.na(Data[[j]]))))
     }
     if (class(Data[[j]])[1]=="POSIXlt" | class(Data[[j]])[1]=="Date") {
@@ -17,7 +31,13 @@ checkData <- function(Data) {
            main=paste0("Missing: ",sum(is.na(Data[[j]]))))
       par(op)
     }
-    if (is.character(Data[[j]]) | is.factor(Data[[j]]) | is.logical(Data[[j]])) print(table(Data[[j]],useNA="ifany"))
+    if (is.character(Data[[j]]) | is.factor(Data[[j]]) | is.logical(Data[[j]])) {
+      if (!any(duplicated(Data[[j]]))) {
+        cat("All values unique: ", Data[[j]][1], ', ', Data[[j]][2], ', ', Data[[j]][3], ', ...\n', sep='')
+      } else {
+        print(table(Data[[j]], useNA="ifany"))
+      }
+    }
     readline("Move on to next variable <press enter>:")
   }
 }
