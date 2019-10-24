83319e0446aa9b26911fcfe1b859d1a4bfcfb567
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-10-24 10:29:31 -0500

    dnplot: Documented and exported

--- R/dnplot.R
+++ R/dnplot.R
@@ -1,3 +1,23 @@
+#' Density plot
+#'
+#' Primarily intended for plotting posterior densities
+#'
+#' @param M                               A vector or matrix of values upon which the density plot is based; if a matrix, separate densities will be estimated and plotted for each column
+#' @param labs                            If supplied, a legend is printed; order of `labs` should correspond to columns of `M`
+#' @param pos                             Are the densities strictly positive?  If so, a boundary correction is applied.  Default: FALSE
+#' @param lwd,bty,lty,col,xlab,ylab,las   Plotting options, as in `par()`
+#' @param dens.par                        Additional arguments to `density()`
+#' @param ...                             Additional arguments to `plot()`
+#'
+#' @examples
+#' M <- cbind(rgamma(1000, 2, 2), rnorm(1000))
+#' dnplot(M[,1])
+#' dnplot(M[,1], pos=TRUE)
+#' dnplot(M)
+#' dnplot(M, labs=c('Gamma', 'Normal'))
+#' dnplot(M, labs=c('Gamma', 'Normal'))
+#' @export
+
 dnplot <- function(M, labs, pos=FALSE, lwd=3, bty="n", lty=1, col=pal(p), ylab="Density", xlab="", las=1, dens.par=NULL, ...) {
   if (!is.matrix(M)) M <- matrix(M, ncol=1)
   p <- ncol(M)
@@ -26,6 +46,6 @@ dnplot <- function(M, labs, pos=FALSE, lwd=3, bty="n", lty=1, col=pal(p), ylab="
     mode[i] <- d$x[which.max(d$y)]
   }
   matplot(X, Y, lty=lty, lwd=lwd, type="l", col=col, ylab=ylab, xlab=xlab, las=1, bty=bty, ...)
-  if (!missing(labs)) toplegend(legend=labs, col=col, lwd=lwd, ncol=min(p, 4))
+  if (!missing(labs)) toplegend(legend=labs, col=col, lwd=lwd)
   invisible(mode)
 }
