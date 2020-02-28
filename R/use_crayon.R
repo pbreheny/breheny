d61251d4cd9ac50eb0c9fe8c03c71cc3c5005049
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-02-28 15:45:01 -0600

    use_crayon(): Fixing minor typo

--- R/use_crayon.R
+++ R/use_crayon.R
@@ -3,7 +3,7 @@
 #' @export
 
 use_crayon <- function() {
-  if (!suppressMessages({requireNamespace("crayon")})) stop("crayon package is not installed", .call=FALSE)
+  if (!suppressMessages({requireNamespace("crayon")})) stop("crayon package is not installed", call.=FALSE)
   options(error = quote(cat(crayon::red$bold((geterrmessage())))), show.error.messages=FALSE)
   options(warning.expression = quote(cat(crayon::magenta$bold(paste0("Warning: ", names(warnings()), ": ", as.character(warnings()), "\n")))))
 }
