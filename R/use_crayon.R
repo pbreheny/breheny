9aa61b70209a9a76bb5e1dd63b4ab4ae7813c43b
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-02-28 10:58:41 -0600

    Fixing DESCRIPTION for use_crayon() to pass Rcheck

--- R/use_crayon.R
+++ R/use_crayon.R
@@ -3,7 +3,7 @@
 #' @export
 
 use_crayon <- function() {
-  suppressMessages({library(crayon)})
-  options(error = quote(cat(red$bold((geterrmessage())))), show.error.messages=FALSE)
-  options(warning.expression = quote(cat(magenta$bold(paste0("Warning: ", names(warnings()), ": ", as.character(warnings()), "\n")))))
+  if (!suppressMessages({requireNamespace("crayon")})) stop("crayon package is not installed", .call=FALSE)
+  options(error = quote(cat(crayon::red$bold((geterrmessage())))), show.error.messages=FALSE)
+  options(warning.expression = quote(cat(crayon::magenta$bold(paste0("Warning: ", names(warnings()), ": ", as.character(warnings()), "\n")))))
 }
