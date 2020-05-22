06f4ca7108eca8902260064d98c0465be3ef7ebd
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-05-22 08:37:29 -0500

    Modifying names of write_fwf arguments

--- R/write-fwf.R
+++ R/write-fwf.R
@@ -5,8 +5,8 @@
 #' @param dt           The data to be printed
 #' @param width        Either a single number or a vector of widths
 #' @param con          Connection, as in `writeLines()`; default is `stdout()`.
-#' @param justify      "l", "r" or something like "lrl" for left, right, left.
-#' @param replace_na   What to print in places of missing values; default: "NA"
+#' @param align        "l", "r" or something like "lrl" for left, right, left.
+#' @param na           What to print in places of missing values; default: "NA"
 #'
 #' @examples
 #' dt <- data.frame(a = 1:3, b = NA, c = c('a', 'b', 'c'))
@@ -14,24 +14,23 @@
 #'
 #' @export
 
-write_fwf = function(dt, width, con=stdout(),
-                     justify = "l", replace_na = "NA") {
+write_fwf = function(dt, width, con=stdout(), align = "l", na = "NA") {
   fct_col = which(sapply(dt, is.factor))
   if (length(fct_col) > 0) {
     for (i in fct_col) {
       dt[[i]] <- as.character(dt[[i]])
     }
   }
-  dt[is.na(dt)] = replace_na
+  dt[is.na(dt)] = na
   n_col = ncol(dt)
-  justify = unlist(strsplit(justify, ""))
-  justify = as.character(factor(justify, c("l", "r"), c("-", "")))
+  align = unlist(strsplit(align, ""))
+  align = as.character(factor(align, c("l", "r"), c("-", "")))
   if (n_col != 1) {
     if (length(width) == 1) width = rep(width, n_col)
-    if (length(justify) == 1) justify = rep(justify, n_col)
+    if (length(align) == 1) align = rep(align, n_col)
   }
   sptf_fmt = paste0(
-    paste0("%", justify, width, "s"), collapse = ""
+    paste0("%", align, width, "s"), collapse = ""
   )
   tbl_content = do.call(sprintf, c(fmt = sptf_fmt, dt))
   tbl_header = do.call(sprintf, c(list(sptf_fmt), names(dt)))
