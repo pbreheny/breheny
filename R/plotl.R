9b14db47ce8bdcb7fc95a4c409cc738d17b041c4
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-08-17 12:11:20 -0500

    plotL(): Now with add and col options

--- R/plotl.R
+++ R/plotl.R
@@ -7,6 +7,7 @@
 #' @param xlab   Default: theta
 #' @param ylab   Default: L(theta)
 #' @param bty    Default: no boundary box
+#' @param add    Add to existing plot?  Default: FALSE
 #' @param ...    Further arguments to `plot()`
 #'
 #' @name likelihood_plot
@@ -25,17 +26,21 @@ NULL
 #'
 #' @export
 
-plotL <- function(x, l, xlab=expression(theta), ylab=expression(L(theta)), bty='n', ...) {
+plotL <- function(x, l, xlab=expression(theta), ylab=expression(L(theta)), bty='n', col, add=FALSE, ...) {
   if (is.matrix(l)) {
+    if (missing(col)) col <- pal(ncol(L))
     L <- apply(l, 2, function(x) x/max(x))
-    matplot(x, L, type='l', xlab=xlab, ylab=ylab, bty=bty,
-            las=1, col=pal(ncol(L)), lwd=3, lty=1)
+    matplot(x, L, type='l', xlab=xlab, ylab=ylab, bty=bty, las=1, col=col, lwd=3, lty=1, add=add)
     if (!is.null(colnames(L))) toplegend(legend=colnames(L), lwd=3, col=pal(ncol(L)))
     return(invisible(L))
   } else {
+    if (missing(col)) col <- pal(2)[2]
     l <- l/max(l)
-    plot(x, l, type='l', xlab=xlab, ylab=ylab, bty=bty,
-         las=1, col=pal(2)[2], lwd=3, ...)
+    if (add) {
+      lines(x, l, xlab=xlab, ylab=ylab, bty=bty, las=1, col=col, lwd=3, ...)
+    } else {
+      plot(x, l, type='l', xlab=xlab, ylab=ylab, bty=bty, las=1, col=col, lwd=3, ...)
+    }
     return(invisible(l))
   }
 }
