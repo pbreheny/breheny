7fdb4ae5dd30fa4f09f08d38a30b1142c6dae3fd
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-09-12 08:29:39 -0500

    ggbox: bug fix

--- R/ggbox.R
+++ R/ggbox.R
@@ -6,13 +6,14 @@
 #'
 #' @examples
 #' ggbox(matrix(rnorm(50), 10, 5))
+#' ggbox(rexp(10), 'A plot of', 'Something')
 #'
 #' @export
 
 ggbox <- function(X, xlab='ind', ylab='values') {
-  if (is.null(dim(X))) remove.label <- TRUE
+  remove.label <- if (is.null(dim(X))) TRUE else FALSE
   if (!is.data.frame(X)) X <- as.data.frame(X)
   p <- ggplot2::ggplot(utils::stack(X), ggplot2::aes_string('ind', 'values')) + ggplot2::geom_boxplot() + ggplot2::xlab(xlab) + ggplot2::ylab(ylab)
-  if (remove.label) p <- p + ggplot2::theme(axis.text.x=ggplot2::element_blank())
+  if (remove.label) p <- p + ggplot2::theme(axis.text.x=ggplot2::element_blank()) + ggplot2::theme(axis.ticks.x=ggplot2::element_blank())
   p
 }
