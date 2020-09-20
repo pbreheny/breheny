61728bad610de23e5ff64f51fefd5db2445a6eb5
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-09-20 07:27:30 -0500

    rasym: For drawing asymptotic paths

---
+++ R/rasym.R
@@ -0,0 +1,31 @@
+#' Generate asymptotic sequence of random normal sums
+#'
+#' Returns the sum of bernoulli variables with different probabilities.
+#'
+#' @param n      Number of random variates to generate.
+#' @param N      Maximum sample size.
+#' @param len    Length of sequence; default: all
+#' @param mu     Mean of normal distribution; default: 0
+#' @param mean   Return means (instead of sums?); default: FALSE
+#'
+#' @examples
+#' rasym(5, 100, 5)
+#' rasym(7, 100, 5, mean=TRUE)
+#'
+#' @export
+#'
+#'
+
+rasym <- function(n, N, len=N, mu=0, mean=FALSE) {
+  if (N %% len != 0) stop('N is not an even multiple of length', call.=FALSE)
+  gap <- floor(N/len) # Should warn?
+  nn <- seq(gap, N, gap)
+  Y <- matrix(rnorm(n*len, sd=sqrt(gap)), len, n) + gap*mu
+  if (mean) {
+    Z <- apply(Y, 2, cumsum)/nn
+  } else {
+    Z <- apply(Y, 2, cumsum)
+  }
+  dimnames(Z) <- list(nn, 1:n)
+  Z
+}
--- R/rdex.R
+++ R/rdex.R
@@ -1,4 +1,16 @@
-rdex <- function(n,mean,var)
-  {
-    return(mean+sample(c(-1,1),n,replace=TRUE)*rexp(n,sqrt(2/var)))
-  }
+#' Generate random double exponential variates
+#'
+#' @param n      Number of random variates to generate.
+#' @param mean   Mean; default: 0
+#' @param var    Variance; default: 1
+#'
+#' @examples
+#' rdex(10)
+#' a <- rdex(10000, mean=3, var=4)
+#' var(a)
+#'
+#' @export
+
+rdex <- function(n, mean=0, var=1) {
+  mean + sample(c(-1,1),n,replace=TRUE) * rexp(n, sqrt(2/var))
+}
