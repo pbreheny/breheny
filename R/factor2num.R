3f9ecea20256117cf2651011eab5c85c2e403ce1
Patrick Breheny <patrick-breheny@uiowa.edu>
2021-02-23 16:06:19 -0600

    factor2num(): Now exported

--- R/factor2num.R
+++ R/factor2num.R
@@ -1,4 +1,9 @@
-factor2num <- function(f)
-  {
-    return(as.numeric(levels(f))[f])
-  }
+#' Convert factor to number
+#'
+#' @param f   Factor with levels like 20, 40, 60, that are simply numbers
+#'
+#' @export
+
+factor2num <- function(f) {
+  return(as.numeric(levels(f))[f])
+}
