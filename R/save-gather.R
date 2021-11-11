7b03c11e3920651525df369f2921e1ac6b807c57
Patrick Breheny <patrick-breheny@uiowa.edu>
2021-11-11 14:07:59 -0600

    bsave(): changing case

--- R/save-gather.R
+++ R/save-gather.R
@@ -1,6 +1,6 @@
 #' Save and combine (gather) results from batched R sessions
 #'
-#' `Bsave()` is run from R; its companion function is to be run through the command line via `gather`, which is located in `Dropbox/bin`.
+#' `bsave()` is run from R; its companion function is to be run through the command line via `gather`, which is located in `Dropbox/bin`.
 #'
 #' @param res      Results to save
 #' @param suffix   By default, files are named 'tmp1.rds', 'tmp2.rds', and so on.  If `suffix='a'`, then files will be named 'tmp1-a.rds', 'tmp2-a.rds', etc.  `gather` will then save results to '2018-11-07-a.rds'.
@@ -11,16 +11,16 @@
 #' A <- rnorm(100)
 #'
 #' # If run interactively
-#' # Bsave(A)        # Saves to 2019-06-14.rds
-#' # Bsave(A, "A")   # Saves to 2019-06-14-A.rds
+#' # bsave(A)        # Saves to 2019-06-14.rds
+#' # bsave(A, "A")   # Saves to 2019-06-14-A.rds
 #'
 #' # If run in batch (with a number as first command line argument)
-#' # Bsave(A)        # Saves to tmp1.rds; gathered to 2019-06-14.rds
-#' # Bsave(A, "A")   # Saves to tmp1-A.rds; gathered to 2019-06-14-A.rds
+#' # bsave(A)        # Saves to tmp1.rds; gathered to 2019-06-14.rds
+#' # bsave(A, "A")   # Saves to tmp1-A.rds; gathered to 2019-06-14-A.rds
 #'
 #' @export
 
-Bsave <- function(res, suffix="") {
+bsave <- function(res, suffix="") {
   Suffix <- if (nchar(suffix) > 0) paste("-",suffix,sep="") else suffix
   NCA <- as.numeric(commandArgs(TRUE))
   if (length(NCA)) {
