49841adb4c9eb7cee3d52bdcd877f4e1bb039492
Patrick Breheny <patrick-breheny@uiowa.edu>
2022-03-15 14:54:45 -0500

    catchWarning(): Adding documentation

--- R/catchWarning.R
+++ R/catchWarning.R
@@ -1,3 +1,14 @@
+#' Catch warnings
+#'
+#' @examples
+#' out <- catchWarning(log('a'))
+#' out$value
+#' out <- catchWarning(as.numeric(c('1', 'a')))
+#' out
+#' out <- catchWarning(1+1)
+#' out
+#' @export
+
 catchWarning <- function(expr) {
   W <- NULL
   w.handler <- function(w){ # warning handler
