93ba0785d4c8dbf0d2780912ba7887ece3bf5f3f
Patrick Breheny <patrick-breheny@uiowa.edu>
2022-04-20 09:43:33 -0500

    qd(): Now exported and documented

--- R/qd.r
+++ R/qd.r
@@ -1,5 +1,14 @@
-## Calculates quadratic forms
-qd <- function(x,A)
-  {
-    crossprod(x,A)%*%x
-  }
+#' Compute a quadratic form (x'Ax)
+#'
+#' @param x   A vector
+#' @param A   A matrix compatible with x
+#'
+#' @examples
+#' x <- rnorm(10)
+#' A <- matrix(rnorm(100), 10, 10)
+#' qd(x, A)
+#' @export
+
+qd <- function(x, A) {
+  drop(crossprod(x, A) %*% x)
+}
