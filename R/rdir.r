78ad1276eccba3953a4d1c7b5f8337a8386f228c
Patrick Breheny <patrick-breheny@uiowa.edu>
2022-10-06 12:45:18 -0500

    2.13: Adding rdir()

---
+++ R/rdir.r
@@ -0,0 +1,16 @@
+#' Generate random variates from the Dirichlet distribution
+#'
+#' @param n      Number of random variates to generate (single integer)
+#' @param alpha  Concentration parameter (vector of positive numbers)
+#'
+#' @returns An `n` by `d` matrix, where `d` is the length of `alpha`.
+#'
+#' @examples
+#' rdir(10, 1:4)
+#' @export
+
+rdir <- function(n, alpha) {
+  d <- length(alpha)
+  Y <- matrix(rgamma(n*d, alpha, 1), d, n)
+  t(sweep(Y, 2, colSums(Y), '/'))
+}
--- R/rdirichlet.r
+++
@@ -1,6 +0,0 @@
-rdirichlet <- function(n, alpha) {
-  l <- length(alpha)
-  x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
-  nn <- x %*% rep(1, l)
-  x/as.numeric(nn)
-}
