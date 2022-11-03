ea1d5dbaaa076bfd0f349dd1d3d57dd5c52e7096
Patrick Breheny <patrick-breheny@uiowa.edu>
2022-11-03 11:57:53 -0500

    event_chart(): New function

---
+++ R/event_chart.r
@@ -0,0 +1,62 @@
+#' Create a time-to-event chart using ggplot
+#'
+#' Inspired by `Hmisc::event.chart()`
+#'
+#' @param Data    A data frame or data.table of dates, organized in the following way (the first two columns are plotted with a line segment, the remaining columns are plotted with a symbol):
+#' * Column 1: Follow-up start date
+#' * Column 2: Follow-up end date
+#' * Additional columns: Additional events that may occur during the follow-up period
+#' @param shift   Shift time so that 0 represents start of follow-up for everyone? (default: FALSE)
+#' @param scale   If shifted, what units? (default: scale=1, meaning days)
+#'
+#' @examples
+#' df <- read.csv(system.file("extdata/cdcaids.txt", package="breheny"))
+#' event_chart(df, FALSE)
+#' event_chart(df, TRUE)
+#' event_chart(df, TRUE, 365.25)
+#' @export
+
+event_chart <- function(Data, shift=FALSE, scale=1) {
+
+  # Convert to data.table and split
+  if (!is.data.table(Data)) {
+    DT <- data.table(Data)
+  } else {
+    DT <- copy(Data)
+  }
+
+  # Check that all columns are dates or can be converted to dates
+  for (j in 1:ncol(DT)) if (!inherits(DT[[j]], 'Date')) DT[[j]] <- as.Date(DT[[j]])
+
+  # Plot
+  v <- names(DT)
+  if (shift) {
+    DT[, (v) := lapply(.SD, function(x) as.numeric(x-DT[[v[1]]])/scale)]
+    DT$y <- rank(DT[[v[2]]], ties.method='first')
+    X <- DT[, c('y', v[1:2]), with=FALSE]
+    names(X) <- c('y', 'x1', 'x2')
+    Y <- melt(DT[, c('y', v[-(1:2)]), with=FALSE], id.vars='y')
+    p <- ggplot2::ggplot(Y, ggplot2::aes_string(x='value', y='y')) +
+      ggplot2::geom_point(ggplot2::aes_string(shape='variable'), na.rm=TRUE) +
+      ggplot2::geom_segment(data=X, ggplot2::aes_string(x='x1', xend='x2', y='y', yend='y')) +
+      ggplot2::xlab('Time on study') +
+      ggplot2::theme(axis.title.y = ggplot2::element_blank(),
+                     axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     legend.title = ggplot2::element_blank())
+  } else {
+    DT$y <- rank(DT[[v[1]]], ties.method='first')
+    X <- DT[, c('y', v[1:2]), with=FALSE]
+    names(X) <- c('y', 'x1', 'x2')
+    Y <- melt(DT[, c('y', v[-(1:2)]), with=FALSE], id.vars='y')
+    p <- ggplot2::ggplot(Y, ggplot2::aes_string(x='value', y='y')) +
+      ggplot2::geom_point(ggplot2::aes_string(shape='variable'), na.rm=TRUE) +
+      ggplot2::geom_segment(data=X, ggplot2::aes_string(x='x1', xend='x2', y='y', yend='y')) +
+      ggplot2::xlab('Date') +
+      ggplot2::theme(axis.title.y = ggplot2::element_blank(),
+                     axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     legend.title = ggplot2::element_blank())
+  }
+  p
+}
--- R/multinom.bayes.r
+++ R/multinom.bayes.r
@@ -4,15 +4,15 @@ multinom.bayes <- function(x, alpha=rep(1, length(x)), level=.95, B=10000) {
     return(multinom.bayes.matrix(x, alpha, level, B))
   }
   if (length(alpha)!=length(x)) stop("Dimension of alpha not consistent with data")
-  pi <- rdirichlet(B, x+alpha)
+  pi <- rdir(B, x+alpha)
   colnames(pi) <- names(x)
   psm(pi)
 }
 multinom.bayes.matrix <- function(X, alpha, level, B) {
   if (nrow(X) > 2) stop("Not set up to handle more than 2 groups yet")
   if (length(alpha)!=ncol(X)) stop("Dimension of alpha not consistent with data")
-  pi1 <- rdirichlet(B, X[1,]+alpha)
-  pi2 <- rdirichlet(B, X[2,]+alpha)
+  pi1 <- rdir(B, X[1,]+alpha)
+  pi2 <- rdir(B, X[2,]+alpha)
   DX <- pi1-pi2
   colnames(DX) <- colnames(X)
   psm(DX)
