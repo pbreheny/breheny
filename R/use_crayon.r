75c178deeece44a1ca0f173eaa0d1cf5112d8c82
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-02-01 09:35:30 -0600

    use_crayon(): Fixing q(), now returns with error code

--- R/use_crayon.r
+++ R/use_crayon.r
@@ -4,7 +4,7 @@
 
 use_crayon <- function() {
   if (!suppressMessages({requireNamespace("crayon")})) stop("crayon package is not installed", call.=FALSE)
-  options(error = quote({cat(crayon::red$bold((geterrmessage()))); if (!interactive()) q()}), show.error.messages=FALSE)
+  options(error = quote({cat(crayon::red$bold((geterrmessage()))); if (!interactive()) q(status=1)}), show.error.messages=FALSE)
   globalCallingHandlers(
     warning = function(cnd) { 
       cnd$message <- crayon::magenta(conditionMessage(cnd))
