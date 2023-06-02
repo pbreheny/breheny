3094df127111b04f976142c45b584ed385bee4ad
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-06-02 16:11:47 -0500

    colorize_terminal(): Now with options to turn off err and warn coloring

--- R/colorize-terminal.r
+++ R/colorize-terminal.r
@@ -1,20 +1,27 @@
 #' Setup errors and warnings to use cli coloring
 #'
+#' @param err    Try to colorize errors? I think this works OK, so default=`TRUE`.
+#' @param warn   Try to colorize warnings? This sometimes screws up the terminal, so set to `FALSE` by default.
+#'
 #' @export
 
-colorize_terminal <- function() {
+colorize_terminal <- function(err=TRUE, warn = FALSE) {
   if (!suppressMessages({
     requireNamespace("cli")})) stop("cli package is not installed", call.=FALSE)
   options(prompt=cli::combine_ansi_styles("cyan", "bold")("> "))
-  options(error = quote({
-    cat(cli::combine_ansi_styles("red", "bold")(geterrmessage()))
-    if (!interactive()) q(status=1)
+  if (err) {
+    options(error = quote({
+      cat(cli::combine_ansi_styles("red", "bold")(geterrmessage()))
+      if (!interactive()) q(status=1)
     }), show.error.messages=FALSE)
-  globalCallingHandlers(
-    warning = function(cnd) {
-      cnd$message <- cli::col_magenta(conditionMessage(cnd))
-      warning(cnd)
-      tryInvokeRestart("muffleWarning")
-    }
-  )
+  }
+  if (warn) {
+    globalCallingHandlers(
+      warning = function(cnd) {
+        cnd$message <- cli::col_magenta(conditionMessage(cnd))
+        warning(cnd)
+        tryInvokeRestart("muffleWarning")
+      }
+    )
+  }
 }
