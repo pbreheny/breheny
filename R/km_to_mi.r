a501cfc0fc6fa422b00460bc432f1d19dacbc9d1
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-06-29 07:28:52 -0500

    km_to_mi(): added distance argument

--- R/km_to_mi.r
+++ R/km_to_mi.r
@@ -4,15 +4,14 @@
 #'
 #' @param m      Minutes
 #' @param s      Seconds (default: 0)
-#' @param half   Per half km? (default: FALSE)
+#' @param dv     Distance (default: 1)
 #'
 #' @examples
 #' km_to_mi(3, 50)
 #' @export
 
-km_to_mi <- function(m, s=0, half=FALSE) {
-  x <- 1.609*(m*60 + s)
-  if (half) x <- x*2
+km_to_mi <- function(m, s=0, d=1) {
+  x <- d * 1.609 * (m*60 + s)
   lubridate::seconds_to_period(round(x))
 }
 
@@ -21,8 +20,7 @@ km_to_mi <- function(m, s=0, half=FALSE) {
 #' mi_to_km(6)
 #' @export
 
-mi_to_km <- function(m, s=0, half=FALSE) {
-  x <- (m*60 + s) / 1.609
-  if (half) x <- x/2
+mi_to_km <- function(m, s=0, d=1) {
+  x <- d * (m*60 + s) / 1.609
   lubridate::seconds_to_period(round(x))
 }
