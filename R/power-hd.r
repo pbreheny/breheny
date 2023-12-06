2af2c09b2b384a010f6b4eb8c420b747764ff87d
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-12-06 10:22:27 -0600

    power_hd(): Fixing small-alpha bug

--- R/power-hd.r
+++ R/power-hd.r
@@ -8,28 +8,37 @@
 #' @param m     Minimum difference among differential features, in terms of mean difference divided by SD (effect size)
 #' @param FDR   Target FDR
 #'
-#' @return A list containing two items:
-#' * `Power`: The power to detect a given feature (function assumes this is the same for all features)
-#' * `Hits`: The total number of expected discoveries (power multiplied by number of differential features)
+#' @return A list containing three items:
+#' * `expected_hits`: Expected number of discoveries (marginal power multiplied by number of differential features)
+#' * `marginal_power`: Power to detect a given feature (function assumes this is the same for all features)
+#' * `disjunctive_power`: Power to detect at least one feature
 #'
 #' @examples
 #' power_hd(10, 1000, 100, 1)
 #' power_hd(seq(10, 100, by=10), 1000, 100, 1)
-#'
+#' power_hd(300, 100000, 50, 0.2)
 #' @export
 
 power_hd <- function(n, p, d, m, FDR=0.1) {
   N <- length(n)
-  Hits <- Power <- numeric(N)
+  expected_hits <- marginal_power <- disjunctive_power <- numeric(N)
   for (i in 1:N) {
-    f <- function(a) {
+    f <- function(log_a) {
+      a <- exp(log_a)
       bb <- tpower(n=2*n[i], delta=m, alpha=a, verbose=FALSE)
       a*p / (a*(p-d) + bb*d) - FDR
     }
-    a <- uniroot(f, c(1e-6, 0.9))$root
-    b <- tpower(n=2*n[i], delta=m, alpha=a, verbose=FALSE)
-    Power[i] <- b
-    Hits[i] <- b*d
+    if (f(-20) > 0) {
+      marginal_power[i] <- expected_hits[i] <- disjunctive_power[i] <- 0
+    } else {
+      log_a <- uniroot(f, c(-20, -0.01))$root
+      b <- tpower(n=2*n[i], delta=m, alpha=exp(log_a), verbose=FALSE)
+      marginal_power[i] <- b
+      expected_hits[i] <- b*d
+      disjunctive_power[i] <- 1-(1-b)^d
+    }
   }
-  list(Hits=Hits, Power=Power)
+  list(expected_hits = expected_hits,
+       marginal_power = marginal_power,
+       disjunctive_power = disjunctive_power)
 }
