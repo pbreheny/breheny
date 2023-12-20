3ff122ceaa535d30632651db54aa8f5a19fec1c1
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-12-20 12:13:06 -0600

    cloud_share -> signed_url

--- R/signed_url.r
+++ R/signed_url.r
@@ -1,7 +1,7 @@
-#' Share Amazon CloudFront file
+#' Share Amazon CloudFront file via presigned url
 #'
-#' Shares Amazon CloudFront file. Currently only works to share teaching files;
-#' see `bin/cloud-share`.
+#' Shares Amazon CloudFront file. Currently only set up for teaching files;
+#' see `bin/signed-url`.
 #'
 #' @returns Either a url or R script. The latter is particularly useful for
 #'   batch downloads and specifying file save paths.
@@ -20,12 +20,12 @@
 #'
 #' @examples
 #' \dontrun{
-#' cloud_share('~/pdf/teaching/7110-f23/dk-1.pdf')
-#' list.files(cloud_share(c('~/web/pdf/BrehenyCV.pdf', '~/web/pdf/mnet.pdf'), r=TRUE)
+#' signed_url('~/pdf/teaching/7110-f23/dk-1.pdf')
+#' signed_url('~/pdf/teaching/7110-f23/dk-*.pdf', r=TRUE)
 #' }
 #' @export
 
-cloud_share <- function(file, out=stdout(), expiration=365, r) {
+signed_url <- function(file, out=stdout(), expiration=365, r) {
   checkmate::assertNumeric(expiration, min.len=1, max.len=1)
   checkmate::assertFileExists(file)
   if (missing(r)) r <- !inherits(out, 'terminal')
@@ -33,7 +33,7 @@ cloud_share <- function(file, out=stdout(), expiration=365, r) {
   cmd <- vector('character', length(file))
   for (i in seq_along(file)) {
     bn <- basename(file[i])
-    cli_cmd <- paste0('cloud-share ', file[i])
+    cli_cmd <- paste0('signed-url ', file[i])
     url <- system(cli_cmd, intern=TRUE)
     if (r) {
       cmd[i] <- paste0("download.file('", url, "', '", bn, "')")
