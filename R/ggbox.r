bbd9ca76685c6fd89e2d4154ccf3826a05eba55e
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-02-29 14:58:58 -0600

    ggbox(): now supports horiz option

--- R/ggbox.r
+++ R/ggbox.r
@@ -6,6 +6,7 @@
 #' @param X       Matrix of values
 #' @param xlab    If omitted, no space will be allocated for x axis title (ylab
 #'                can be specified as usual; see examples)
+#' @param horiz   Make horizontal? (default: FALSE)
 #'
 #' @examples
 #' ggbox(matrix(rnorm(50), 10, 5))
@@ -18,9 +19,11 @@
 #' attr(X[[4]], 'label') <- "The petal's width"
 #' ggbox(X)
 #' ggbox(X[,4])
+#' ggbox(X[,4], horiz=TRUE)
+#' ggbox(X, horiz=TRUE)
 #' @export
 
-ggbox <- function(X, xlab) {
+ggbox <- function(X, xlab, horiz=FALSE) {
   if (!is.data.frame(X)) X <- as.data.frame(X)
   df <- lapply(X, as.vector) |> stack()
   df$ind <- as.character(df$ind)
@@ -29,17 +32,31 @@ ggbox <- function(X, xlab) {
       df$ind[df$ind == v] <- attr(X[[v]], 'label')
     }
   }
-  p <- ggplot2::ggplot(df, ggplot2::aes(.data$ind, .data$values)) +
-    ggplot2::geom_boxplot()
-  if (missing(xlab)) {
-    p <- p + ggplot2::theme(axis.title.x = ggplot2::element_blank())
+  if (horiz) {
+    p <- ggplot2::ggplot(df, ggplot2::aes(.data$values, .data$ind))
+    if (missing(xlab)) {
+      p <- p + ggplot2::theme(axis.title.y = ggplot2::element_blank())
+    } else {
+      p <- p + ggplot2::ylab(xlab)
+    }
+    if (length(unique(df$ind)) == 1) {
+      p <- p + ggplot2::theme(axis.text.y = ggplot2::element_blank()) +
+        ggplot2::theme(axis.ticks.y = ggplot2::element_blank()) +
+        ggplot2::xlab(df$ind[1])
+    }
   } else {
-    p <- p + ggplot2::xlab(xlab)
+    p <- ggplot2::ggplot(df, ggplot2::aes(.data$ind, .data$values))
+    if (missing(xlab)) {
+      p <- p + ggplot2::theme(axis.title.x = ggplot2::element_blank())
+    } else {
+      p <- p + ggplot2::xlab(xlab)
+    }
+    if (length(unique(df$ind)) == 1) {
+      p <- p + ggplot2::theme(axis.text.x=ggplot2::element_blank()) +
+        ggplot2::theme(axis.ticks.x=ggplot2::element_blank()) +
+        ggplot2::ylab(df$ind[1])
+    }
   }
-  if (length(unique(df$ind)) == 1) {
-    p <- p + ggplot2::theme(axis.text.x=ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.x=ggplot2::element_blank()) +
-      ggplot2::ylab(df$ind[1])
-  }
+  p <- p + ggplot2::geom_boxplot()
   p
 }
