2d2d1073190d2e4851633b82e71c2f173657e711
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-03-28 16:16:39 -0500

    samsize_pcc(): Fixing a bug, adding documentation, making much faster

--- R/samsize_pcc.r
+++ R/samsize_pcc.r
@@ -1,58 +1,64 @@
 #' Sample size for classifier development
 #'
-#' Targets the probability of correct classification (PCC) to within a certain tolerance of the optimal PCC.
+#' Determine the sample size necessary to estimate the probability of correct
+#' classification (PCC) to within a certain tolerance of the optimal (Bayes)
+#' PCC.
 #'
-#' Loosely based on the function `MKmisc::ssize.pcc()`, but more accurate.  I think it is also more accurate than the results presented by Dobbin (2008), but I'm not 100% clear on their method when n1 != n2.  When n1=n2, all methods agree.
+#' Assumes a multivariate normal distribution with spherical variance.
+#' Loosely based on the function `MKmisc::ssize.pcc()`, but with two primary
+#' differences:
+#' 1. Doesn't solve for worst case scenario over 1:dfeat, just uses dfeat.
+#' 2. Uses `tpower()` rather than approximating the power of the t-test.
 #'
-#' @param tolerance   Sample size is found such that the absolute value of \eqn{PCC(\infty) - PCC(n)} is less than `tolerance`.
-#' @param stdfc       Standardized fold change (difference in means on log scale divided by standard deviation)
+#' @param effect      Effect size (difference in means divided by SD)
+#' @param tolerance   Sample size is found such that \eqn{PCC(\infty) - PCC(n)} is less than `tolerance`.
 #' @param p           Proportion of less common class (default 0.5)
 #' @param nfeat       Number of features
-#' @param dfeat       Number of differential features
+#' @param dfeat       Number of differential features. Note that Dobbin & Simon recommend using dfeat=1.
 #'
 #' @return Object of class `power.htest``, a list of the arguments augmented with method and note elements.
 #'
 #' @examples
-#' samsize_pcc(0.1, 2, 0.3, 22000)
-#'
+#' samsize_pcc(0.5, 0.001)
+#' samsize_pcc(1, 0.1, nfeat=22000)
+#' samsize_pcc(0.8, 0.1, p=1/3, nfeat=22000, dfeat=20)
 #' @export
 
-samsize_pcc <- function(tolerance, stdfc, p = 0.5, nfeat, dfeat = 20){
-  pcc.n.a <- function(alpha, n, stdfc, nfeat, m){
-    pow <- tpower(n1=n[1], n2=n[2], delta=stdfc, alpha = alpha)
-    T1 <- stdfc*m*pow/2
-    T2 <- sqrt(m*pow + alpha*(nfeat-m))
-    pnorm(T1/T2)
-  }
+samsize_pcc <- function(effect, tolerance, p = 0.5, nfeat = 1, dfeat = 1) {
+  delta <- effect/2
+  q <- 1 - p
+  k <- 1/2 * log((1-p)/p)
+  m <- dfeat
   n1 <- 2
-  M <- dfeat
   repeat {
     n1 <- n1 + 1
     n2 <- floor(n1*(1-p)/p)
-    #n2 <- n2 + 1
-    #n1 <- floor(n2*p/(1-p))
-    #if (n1 < 2) next
     n <- c(n1, n2)
-    m <- seq_len(M)
-    alpha <- numeric(M)
-    for(mi in m){
-      alpha[mi] <- optimize(f = pcc.n.a, interval = c(0, 1), maximum = TRUE,
-                            n = n, stdfc = stdfc, nfeat = nfeat, m = mi,
-                            tol = 1e-10)$maximum
-    }
-    pow <- tpower(n1=n[1], n2=n[2], delta=stdfc, alpha = alpha)
-    T1 <- stdfc*m*pow/2
-    T2 <- sqrt(m*pow + alpha*(nfeat-m))
-    crit <- max(pnorm(stdfc*sqrt(m)/2) - pnorm(T1/T2))
-    if (crit < tolerance) break
+    pcc_n <- optimize(f = pcc_lower_bound, interval = c(0, 1), maximum = TRUE,
+                      n = n, delta = delta, nfeat = nfeat, m = m, p=p,
+                      tol = 1e-10)$objective
+    pcc_inf <- p * pnorm((m * delta^2 - k) / sqrt(m * delta^2)) + q * pnorm((m * delta^2 + k) / sqrt(m * delta^2))
+    if (pcc_inf - pcc_n < tolerance) break
   }
-  METHOD <- "Sample Size Planning for Developing Classifiers Using High Dimensional Data"
 
-  res <- structure(list(tolerance = tolerance, p = p,
-                        stdfc = stdfc,
-                        nfeat = nfeat,
-                        n1 = n[1], n2 = n[2],
-                        method = METHOD),
-                   class = "power.htest")
-  res
+  structure(
+    list(
+      tolerance = tolerance,
+      p = p,
+      effect = effect,
+      nfeat = nfeat,
+      dfeat = dfeat,
+      pcc_inf = pcc_inf,
+      pcc_n = pcc_n,
+      n1 = n[1], n2 = n[2],
+      method = "Sample Size Planning for Developing Classifiers Using High Dimensional Data"),
+    class = "power.htest")
+}
+
+pcc_lower_bound <- function(alpha, n, delta, nfeat, m, p) {
+  k <- 1/2 * log((1-p)/p)
+  pow <- tpower(n1=n[1], n2=n[2], delta=2*delta, alpha = alpha, verbose=FALSE)
+  q <- 1 - p
+  p*pnorm((delta * m * pow - k) / sqrt(m * pow + (nfeat - m) * alpha)) +
+    q*pnorm((k + delta * m * pow) / sqrt(m * pow + (nfeat - m) * alpha))
 }
--- inst/tinytest/_samsize_pcc.r
+++ inst/tinytest/_samsize_pcc.r
@@ -1,5 +1,6 @@
 if (interactive()) library(tinytest)
 gamma <- 0.01
 effect <- 1
-samsize_pcc(1, 0.1, nfeat=22000, dfeat=20)
+# These two are not exactly the same; see `?samsize_pcc` for details
+samsize_pcc(1, 0.1, nfeat=22000, dfeat=1)
 MKmisc::ssize.pcc(0.1, 1, nrFeatures = 22000, sigFeatures = 20, verbose = FALSE)
