9d03a4ecad37e32dffbaf86240a5bc6c3a1710bc
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-04-22 13:18:10 -0500

    .p(): like .f(), but percent

--- R/f.r
+++ R/f.r
@@ -1,5 +1,7 @@
 #' Compact and pipe-friendly wrapper to sprintf
 #'
+#' .p is the same, but multiplies by 100 first (for percents)
+#'
 #' @param x        A floating point number
 #' @param digits   Number of decimals to display
 #'
@@ -12,4 +14,10 @@
     sprintf(x) |>
     as.numeric() |>
     format(big.mark = ",", nsmall=digits)
+}
+
+#' @rdname .f
+#' @export
+.p <- function(x, digits) {
+  .f(100*x, digits)
 }
--- inst/tinytest/test-f.r
+++ inst/tinytest/test-f.r
@@ -4,3 +4,6 @@ if (interactive()) library(tinytest)
 0.10000 |> .f(2) |> expect_equal('0.10')
 123456.789 |> .f(1) |> expect_equal('123,456.8')
 123456.700 |> .f(2) |> expect_equal('123,456.70')
+
+0.12345 |> .p(1) |> expect_equal('12.3')
+0.12000 |> .p(2) |> expect_equal('12.00')
