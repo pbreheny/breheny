374e928e698b18ef12f06a0602ba17cb6191f6d1
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-04-23 07:31:37 -0500

    .fm(): adding {,} escapes for math mode

--- R/f.r
+++ R/f.r
@@ -1,6 +1,8 @@
 #' Compact and pipe-friendly wrapper to sprintf
 #'
 #' `.p()` is the same as `.f()`, but multiplies by 100 first (for percents).
+#' `.fm()` is the same as `.f()`, but puts braces around commas for use in
+#' latex math mode.
 #'
 #' @param x        A floating point number
 #' @param digits   Number of decimals to display
@@ -9,6 +11,8 @@
 #'
 #' @examples
 #' runif(1) |> .f(2)
+#' runif(1) |> .p(2)
+#' runif(1, 100000, 200000) |> .fm(2)
 #' @export
 
 .f <- function(x, digits) {
@@ -22,4 +26,10 @@
 #' @export
 .p <- function(x, digits) {
   .f(100*x, digits)
+}
+
+#' @rdname dot-f
+#' @export
+.fm <- function(x, digits) {
+  gsub(',', '{,}', .f(x, digits))
 }
