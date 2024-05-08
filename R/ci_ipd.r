7e650d9650b1620b6ba7f83671ab61725bec76b2
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-05-08 08:32:10 -0500

    ci_ipd(): A function to create individual data from a CI

---
+++ R/ci_ipd.r
@@ -0,0 +1,20 @@
+#' Given a confidence interval, simulate individual patient data
+#'
+#' @param ci   Confidence interval
+#' @param n    Sample size
+#' @param df   Degrees of freedom (default: infinite)
+#'
+#' @returns A numeric vector of individual patient data
+#'
+#' @examples
+#' ci_ipd(c(-0.31, -0.02), 10)
+#' @export
+
+ci_ipd <- function(ci, n, df=Inf) {
+  se <- abs(ci[2] - ci[1]) / (2 * qt(0.975, df))
+  sd <- se * sqrt(n)
+  m <- mean(ci)
+  list(mean = m,
+       sd   = sd,
+       ipd  = rnorm(n, m, sd))
+}
