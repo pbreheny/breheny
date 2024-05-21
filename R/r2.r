97420faeea6011145a8a617ca142021ad1f354ae
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-05-21 08:20:59 -0500

    r2: adding mcfadden r2

--- R/r2.r
+++ R/r2.r
@@ -2,15 +2,21 @@
 #'
 #' @param fit Output of [glm()]
 #'
+#' @returns A named vector:
+#' * **mf:** McFadden \eqn{R^2}
+#' * **cs:** Cox-Snell \eqn{R^2}
+#' * **ng:** Nagelkerke \eqn{R^2}
+#'
 #' @examples
 #' fit <- glm(am ~ hp + wt, data = mtcars, family = binomial)
 #' r2(fit)
 #' @export
 
 r2 <- function(fit) {
+  mf <- 1 - fit$deviance/fit$null.deviance
   n <- length(fit$y)
   cs <- 1 - exp(-(fit$null.deviance - fit$deviance) / n)
   l0 <- 1 - exp(-fit$null.deviance / n)
   ng <- cs / l0
-  c(cs=cs, ng=ng)
+  c(mf=mf, cs=cs, ng=ng)
 }
