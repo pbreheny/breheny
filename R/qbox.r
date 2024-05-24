9d88b5b0ca64998361d9013cf353f853299c08dc
Patrick Breheny <patrick-breheny@uiowa.edu>
2024-05-24 12:48:11 -0500

    qbox(): now accepts probs option

--- R/qbox.r
+++ R/qbox.r
@@ -3,21 +3,23 @@
 #' @param df           A data frame
 #' @param x            The continuous variable
 #' @param g            The categorical (group) variable
+#' @param probs        Quantiles at which to draw box and whiskers (default: `c(0.1, 0.25, 0.5, 0.75, 0.9)`)
 #' @param orientation  Either `horizontal` (default) or `vertical`
 #'
 #' @examples
 #' qbox(iris, Sepal.Length, Species)
 #' qbox(iris, Sepal.Length, Species, orientation='vertical')
+#' qbox(iris, Sepal.Length, Species, c(0, 0.25, 0.5, 0.75, 1))
 #' @export
 
-qbox <- function(df, x, g, orientation=c('horizontal', 'vertical')) {
+qbox <- function(df, x, g, probs = c(0.1, 0.25, 0.5, 0.75, 0.9), orientation=c('horizontal', 'vertical')) {
   orientation <- match.arg(orientation)
   dat <- eval(substitute(as.data.table(df)[, .(
-    min = quantile(x, 0.1),
-    low = quantile(x, 0.25),
-    mid = quantile(x, 0.5),
-    hi  = quantile(x, 0.75),
-    max = quantile(x, 0.9)),
+    min = quantile(x, probs[1]),
+    low = quantile(x, probs[2]),
+    mid = quantile(x, probs[3]),
+    hi  = quantile(x, probs[4]),
+    max = quantile(x, probs[5])),
     g]))
   if (orientation == 'horizontal') {
       ggplot2::ggplot(dat, ggplot2::aes(y={{g}}, fill={{g}})) +
