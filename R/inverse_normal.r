0fd4dedc4f2c4c37b653ee0aed79a58b7e1e1f94
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-06-02 11:25:44 -0500

    inverse_normal: new function for transformation

---
+++ R/inverse_normal.r
@@ -0,0 +1,12 @@
+#' Apply inverse normal rank transformation
+#'
+#' @param x  Vector of numeric values
+#'
+#' @examples
+#' c(-5:5, NA, NA, NA) |> inverse_normal()
+#' @export
+inverse_normal <- function(x) {
+  na_idx <- is.na(x)
+  qx <- (rank(x, na.last = 'keep') - .5) / (length(x) - sum(na_idx))
+  qnorm(qx)
+}
